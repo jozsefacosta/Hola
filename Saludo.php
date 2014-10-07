5b8da5f24f0c943c373a57baeeb11d902543f07a
jozsefacosta <jozsef3@hotmail.com>
2014-10-06 21:20:55 -0500

    Commit

--- Saludo.php
+++ Saludo.php
@@ -1,7 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
 
-<?php
+<html>
 
-echo "Hola Mundo";
+<head>
+<title>Hola Mundo</title>
+</head>
+
+
+
+<body>
+
+<h1>Hola Mundo</h1>
+</body>
+
+</html>
 
